29129d49787b00111fb944222273344fbbdc6545
Sallyzahran <sallyzahran.sz@gmail.com>
2023-06-03 20:29:37 +0300

    UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { BookApiService } from './../services/book-api.service';
+import { BookInterface } from './../interfaces/book-interface';
+import { Author } from '../interfaces/author';
+import { AuthorApiService } from '../services/author-api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  bookList!:BookInterface[];
+  authorList!: Author[];
+
+constructor(private bookService:BookApiService,private authorApiService: AuthorApiService) {
+  this.bookService.getBooks().subscribe(value=>this.bookList=value.slice(0, 4))  
+  this.authorApiService.getAllAuthors().subscribe(value => this.authorList = Object.values(value).slice(0, 4));
+}
 
 }
