9ae7252b554b9f206952ebde7f33306223a15c07
Sallyzahran <sallyzahran.sz@gmail.com>
2023-06-03 03:08:23 +0300

    add auth

--- src/app/services/book-api.service.ts
+++ src/app/services/book-api.service.ts
@@ -1,53 +1,67 @@
 import { BookInterface } from './../interfaces/book-interface';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookApiService {
+
+  localStorage !: Storage;
+  headers!:HttpHeaders;
 private path = 'http://localhost:5000/book'
   // private allBooks=new BehaviorSubject<BookInterface[]>([]);
   // Books=this.allBooks.asObservable();
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,private router:Router) { 
+
+    this.localStorage = window.localStorage;
+    this.headers = new HttpHeaders()
+   .set('x-access-token',String(this.localStorage.getItem('token')))
+
+  }
+
+  private authUpdated = new BehaviorSubject<object>({})
+  authorUpdated = this.authUpdated.asObservable();
  getBooks(){
-  return this.http.get<BookInterface[]>(this.path);
+  return this.http.get<BookInterface[]>(this.path,{headers:this.headers});
  }
 
  getBook(id:string){
-  return this.http.get<BookInterface>(this.path+`/${id}`)
+  return this.http.get<BookInterface>(this.path+`/${id}`,{headers:this.headers})
 }
 
   addBook(body:any){
 
-return this.http.post(this.path,body);
+return this.http.post(this.path,body,{headers:this.headers});
 }
 
 getAuthors(){
-  return this.http.get('http://localhost:5000/author');
+  return this.http.get('http://localhost:5000/author',{headers:this.headers});
 }
 
 getCategories(){
-  return this.http.get('http://localhost:5000/category');
+  return this.http.get('http://localhost:5000/category',{headers:this.headers});
 }
 
 updateBook(body:any,id:string){
-  return this.http.put(this.path+`/${id}`,body);
+  return this.http.put(this.path+`/${id}`,body,{headers:this.headers});
 }
 
 deleteBook(id:string){
 
-  return this.http.delete(this.path+`/${id}`)
+  return this.http.delete(this.path+`/${id}`,{headers:this.headers})
 }
 //delete userId From Uri add token to header :D
 getUserBook(userId:string){
-return this.http.get(`http://localhost:5000/mybook/${userId}`)
+return this.http.get(`http://localhost:5000/mybook/${userId}`,{headers:this.headers})
 }
 
 
 addReview(id: string, body: any) {
-  return this.http.post(`http://localhost:5000/review/${id}`, body);
+  return this.http.post(`http://localhost:5000/review/${id}`, body,{headers:this.headers});
 
 
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,6 +1,7 @@
+import { Router } from '@angular/router';
 
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient , HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Category } from '../interfaces/category';
 import { Book } from '../interfaces/category';
@@ -11,32 +12,63 @@ import { Book } from '../interfaces/category';
 export class CategoryService {
 
 
-  constructor(private http:HttpClient) { }
+    localStorage !: Storage;
+  headers!:HttpHeaders;
+
+  constructor(private http:HttpClient,private router:Router) { 
+
+    this.localStorage = window.localStorage;
+     this.headers = new HttpHeaders()
+    .set('x-access-token',String(this.localStorage.getItem('token')))
+
+  }
+    private authUpdated = new BehaviorSubject<object>({})
+  authorUpdated = this.authUpdated.asObservable();
+
 
   getCategories(){
-    return this.http.get<Category[]>(`http://localhost:5000/category/`);
+    return this.http.get<Category[]>(`http://localhost:5000/category/`,{headers:this.headers});
   }
 
 
   getCategoryBooks(id: string){
-    return this.http.get<Book[]>(`http://localhost:5000/category/${id}/book`);
+    return this.http.get<Book[]>(`http://localhost:5000/category/${id}/book`,{headers:this.headers});
   }
 
   deleteCategory(id: string){
-    return this.http.delete(`http://localhost:5000/category/${id}`);
+    if(this.localStorage.getItem("isAdmin"))
+
+    return this.http.delete(`http://localhost:5000/category/${id}`,{headers:this.headers});
+    else{
+      this.router.navigate(['**']);
+      return this.authUpdated
+    }
   }
 
   addCategory(category: any) {
-    return this.http.post(`http://localhost:5000/category/`, category);
+    if(this.localStorage.getItem("isAdmin"))
+
+    return this.http.post(`http://localhost:5000/category/`, category,{headers:this.headers});
+    else{
+      this.router.navigate(['**']);
+      return this.authUpdated
+    }
   }
 
   getCategiryId(id:string): Observable<any> {
-    return this.http.get<Category>(`http://localhost:5000/category/${id}`);
+    return this.http.get<Category>(`http://localhost:5000/category/${id}`,{headers:this.headers});
+    
 
   }
 
   UpdateCategory(id: string, category:any) {
-    return this.http.put(`http://localhost:5000/category/${id}`, category);
+    if(this.localStorage.getItem("isAdmin"))
+
+    return this.http.put(`http://localhost:5000/category/${id}`, category,{headers:this.headers});
+    else{
+      this.router.navigate(['**']);
+      return this.authUpdated
+    }
   }
 
 
