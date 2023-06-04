499bb2303370cff1ec2cdd5592bb4be34fe49f78
Sallyzahran <sallyzahran.sz@gmail.com>
2023-06-04 17:54:39 +0300

    message

--- src/app/admin-category/admin-category.component.ts
+++ src/app/admin-category/admin-category.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Category } from '../interfaces/category';
 import { CategoryService } from '../services/category.service';
 import { Router } from '@angular/router';
-
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-admin-category',
@@ -26,16 +26,26 @@ this.categoryService.getCategories().subscribe((res:any) => this.categories=res)
 }
 
 
+
 deleteCategory(_id: string) {
-  this.categoryService.deleteCategory(_id)
-    .subscribe(() => {
+  this.categoryService.deleteCategory(_id).subscribe((res: any) => {
+    if (res && res.message == 'Unable to delete category because there are books associated with this category') {
+      Swal.fire({
+        icon: 'error',
+        title: 'Unable to delete category',
+        text: 'There are books associated with this category',
+        confirmButtonText: 'OK'
+      });
+    } else {
       this.categories = this.categories.filter((category) => category._id !== _id);
-
+    }
     });
-
-
+  }
+    
   
-}
+
+
+
 
 editCategory( id:any){
 
